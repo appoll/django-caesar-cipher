f9aa493451876053ebd96f23fc23cd24aecede98
appoll <paul.antton@gmail.com>
2016-03-28 15:21:57 +0200

    Write tests for CaesarCipher class.
    Tests already useful in proving that basic encryption decryption only works with lower-cased-one-worded inputs.

--- caesarapp/tests.py
+++ caesarapp/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class CaesarCipherTestCase(TestCase):
+    def setUp(self):
+        key = random.randrange(0, 100)
+        self.message = "onelowercaseword"
+        self.testCipher = CaesarCipher(key)
+
+    def test_encrypt_decrypt(self):
+        encrypted = self.testCipher.encrypt(self.message)
+        decrypted = self.testCipher.decrypt(encrypted)
+        self.assertEqual(self.message, decrypted)
