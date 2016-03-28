18d2ddef43e1b02c7692e46234db41df357b6b21
appoll <paul.antton@gmail.com>
2016-03-28 16:54:13 +0200

    Add time field to user request. Make and run migration to update database

--- caesarapp/models.py
+++ caesarapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class CaesarCipher:
@@ -25,6 +25,7 @@ class CaesarCipher:
 class UserInput(models.Model):
     plain_text = models.CharField(max_length=140)
     key = models.IntegerField(default=0)
+    pub_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.plain_text
