227334b2a949f1aab6c1b7a2091847e2562e43f4
appoll <paul.antton@gmail.com>
2016-03-29 17:23:58 +0200

    Add view for caesar index and map it to an URL
    A view is just a function; it contains logic to provide a response based on the request received as a parameter

--- caesarapp/views.py
+++ caesarapp/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def indexCaesarApp(request):
+    return HttpResponse("Hello World, this is the CaesarApp index.")
