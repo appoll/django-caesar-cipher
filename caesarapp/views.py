e8c12edd2a5e911851468505760f55e172b4a2f4
appoll <paul.antton@gmail.com>
2016-03-30 19:55:11 +0200

    Create first basic template and make the view responsible for displaying it.

--- caesarapp/views.py
+++ caesarapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, 'caesarapp/index.html', {})
 
 def about(request):
     return HttpResponse("Hello Universe")
