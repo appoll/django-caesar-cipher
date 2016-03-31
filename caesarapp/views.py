6fef712c61be1f33b86e3277260e12d28c96b79f
appoll <paul.antton@gmail.com>
2016-03-31 20:35:20 +0200

    Create & link encrypt view, url & template.

--- caesarapp/views.py
+++ caesarapp/views.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils import timezone
+from .forms import UserInputForm
 
 
 # Create your views here.
 
 def index(request):
     return render(request, 'caesarapp/index.html', {})
+
+
+def encrypt(request):
+    return render(request, 'caesarapp/encrypt.html', {})
+
 
 def about(request):
     return HttpResponse("Hello Universe")
