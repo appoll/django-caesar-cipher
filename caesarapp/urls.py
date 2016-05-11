de53849835c8b9b4b0c97bad2c382543890e65a4
appoll <paul.antton@gmail.com>
2016-05-11 20:04:57 +0200

    raw message in the url looks like a pretty bad design decision

--- caesarapp/urls.py
+++ caesarapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^encrypt/$', views.encrypt, name='encrypt'),
+    url(r'^(?P<raw_message>[a-z]*)/encrypted/$', views.encrypted, name='encrypted'),
 ]
--- caesarapp/views.py
+++ caesarapp/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.utils import timezone
 from .forms import UserInputForm
-
+import pdb
 
 # Create your views here.
 
@@ -17,9 +18,16 @@ def encrypt(request):
             userInput = form.save(commit=False)
             userInput.pub_date = timezone.now()
             userInput.save()
+            pdb.set_trace()
+            return HttpResponseRedirect(reverse('encrypted', args=(form.cleaned_data['plain_text'], )))
     else:
         form = UserInputForm()
     return render(request, 'caesarapp/encrypt.html', {'form': form})
+
+def encrypted(request, raw_message):
+    pdb.set_trace()
+    response = "You're looking at the encryption of your message \"%s\"."
+    return HttpResponse(response % raw_message)
 
 
 def about(request):
