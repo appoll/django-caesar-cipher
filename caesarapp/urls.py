3974e60383903123f3e032875b39f1d588786934
appoll <paul.antton@gmail.com>
2016-05-12 19:34:50 +0200

    replace raw message with userInput id

--- caesarapp/urls.py
+++ caesarapp/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^encrypt/$', views.encrypt, name='encrypt'),
-    url(r'^(?P<raw_message>[a-z]*)/encrypted/$', views.encrypted, name='encrypted'),
+    url(r'^(?P<userInput_id>[0-9]+)/encrypted/$', views.encrypted, name='encrypted'),
 ]
--- caesarapp/views.py
+++ caesarapp/views.py
@@ -1,7 +1,9 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+
+from .models import UserInput
 from .forms import UserInputForm
 import pdb
 
@@ -19,15 +21,16 @@ def encrypt(request):
             userInput.pub_date = timezone.now()
             userInput.save()
             pdb.set_trace()
-            return HttpResponseRedirect(reverse('encrypted', args=(form.cleaned_data['plain_text'], )))
+            return HttpResponseRedirect(reverse('encrypted', args=(userInput.id, )))
     else:
         form = UserInputForm()
     return render(request, 'caesarapp/encrypt.html', {'form': form})
 
-def encrypted(request, raw_message):
+def encrypted(request, userInput_id):
     pdb.set_trace()
+    userInput = get_object_or_404(UserInput, pk=userInput_id)
     response = "You're looking at the encryption of your message \"%s\"."
-    return HttpResponse(response % raw_message)
+    return HttpResponse(response % userInput.plain_text)
 
 
 def about(request):
